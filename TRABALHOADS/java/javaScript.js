eeb2e493c4306354dfd8f73cc7f4699fd0311edf
Fernandes Morais <fernandesmorais@outlook.com>
2025-06-25 12:01:39 -0300

    Add files via upload

---
+++ TRABALHOADS/java/javaScript.js
@@ -0,0 +1,6 @@
+let botao = document.querySelector("#botao");
+<script>
+    document.getElementById("Botao").addEventListener("click", function() {
+      alert("Olá! Você clicou no botão!");
+    });
+  </script>
